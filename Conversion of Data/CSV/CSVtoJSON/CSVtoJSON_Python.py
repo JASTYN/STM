a94592a8675ac100f7572eea5571f12aa04531c2
JASTYN <jastynmanya@gmail.com>
2022-03-27 13:30:51 +0300

    conversion and crud function

--- Conversion of Data/CSV/CSVtoJSON/CSVtoJSON_Python.py
+++ Conversion of Data/CSV/CSVtoJSON/CSVtoJSON_Python.py
@@ -5,14 +5,14 @@ import json
 cd = os.path.dirname(os.path.abspath(__file__))
 
 # CSV IMPORT
-with open(os.path.join(cd, 'CLData.csv')) as csvfile:    
+with open(os.path.join(cd, 'MOCK_DATA.csv')) as csvfile:    
     reader = csv.DictReader(csvfile)
     data = [row for row in reader]
 
 jsondata = json.dumps(data, indent=4)
 
 # JSON EXPORT
-with open(os.path.join(cd, 'CLData_Py.json'),'w') as jsonfile:
+with open(os.path.join(cd, 'MOCK_DATA.json'),'w') as jsonfile:
     jsonfile.write(jsondata)    
 
 print("Successfully migrated CSV data to JSON!")
--- Conversion of Data/JSON/JSONtoCSV/JSONtoCSV_Python.py
+++ Conversion of Data/JSON/JSONtoCSV/JSONtoCSV_Python.py
@@ -3,11 +3,11 @@ import json, csv
 
 # READ JSON
 cd = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(cd,'CLData.json')) as data_file:    
+with open(os.path.join(cd,'MOCK_DATA.json')) as data_file:    
     data = json.load(data_file)
 
 # WRITE TO CSV
-with open(os.path.join(cd, 'CLData_Py.csv'), 'w', newline='') as csvfile:
+with open(os.path.join(cd, 'MOCK_DATA.csv'), 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(list(data[0].keys()))  
 
